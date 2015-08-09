322fc4d71f7c2f6eb11d4d6828efb5e54f282180
Armin Sander <armin@replicator.org>
2015-08-09 23:28:52 +0200

    Setting that disables downloads and uploads from and to other PCs

--- Dominator.Windows10/Settings/Annoyances.cs
+++ Dominator.Windows10/Settings/Annoyances.cs
@@ -13,10 +13,19 @@ namespace Dominator.Windows10.Settings
 		public static GroupBuilder Annoyances(this GroupBuilder dsl) => dsl
 			.BeginGroup("Annoyances")
 			.Explanation("Settings that are very annoying")
+
 				.BeginItem("Skype Home and Advertising")
 				.Explanation("Block all Skype home and advertising URLs in the system's hosts file")
 				.Hosts("Settings/skype-ads.txt")
 				.End()
+
+				// https://techjourney.net/enable-or-disable-peer-to-peer-p2p-apps-updates-download-from-more-than-one-place-in-windows-10/
+
+				.BeginItem("Windows Update")
+				.Explanation("Never get updates from or send updates to other PCs")
+				.RegistryValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config", "DODownloadMode", 0, 3)
+				.End()
+
 			.End();
 	}
 }
