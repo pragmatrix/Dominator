615f1d38e65b8fa9267ebaa4d04cba530e4d26b1
Armin Sander <armin@replicator.org>
2015-08-15 18:04:31 +0200

    move Item and Group classes in Model.cs

--- Dominator.Net/DSL.cs
+++ Dominator.Net/DSL.cs
@@ -133,43 +133,4 @@ namespace Dominator.Net
 			return dominator;
 		}
 	}
-
-	sealed class Item : IDominatorItem
-	{
-		internal Item(
-			DominatorDescription description, 
-			Func<DominatorState> getter, 
-			Action<DominationAction> setter)
-		{
-			Description = description;
-			_getter = getter;
-			_setter = setter;
-		}
-
-		readonly Func<DominatorState> _getter;
-		readonly Action<DominationAction> _setter;
-
-		public DominatorDescription Description { get; }
-		public DominatorState GetState()
-		{
-			return _getter();
-		}
-
-		public void SetState(DominationAction action)
-		{
-			_setter(action);
-		}
-	}
-
-	sealed class Group : IDominatorGroup
-	{
-		public Group(DominatorDescription description, IDominator[] nested)
-		{
-			Description = description;
-			Nested = nested;
-		}
-
-		public DominatorDescription Description { get; }
-		public IDominator[] Nested { get; }
-	}
 }
